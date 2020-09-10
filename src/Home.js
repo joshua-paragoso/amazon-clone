934ecfc1e3035b7cc18cf81e2c75ee2f322a3fab
joshypuu <paragoso90@gmail.com>
2020-09-10 09:45:06 -1000

    Each product is unique with paramter data

--- src/Home.js
+++ src/Home.js
@@ -12,17 +12,49 @@ function Home() {
         />
 
         <div className="home__row">
-          <Product />
+          <Product 
+          title="The lean start up"
+          price={29.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+          rating={5}
+          />
+          <Product
+           id="12345"
+           title="GTX 3080 Graphics card"
+           price={359.99}
+           image="https://i.redd.it/dmd6lf9jdr851.jpg"
+           rating={4}
+          />
           {/*product*/}
         </div>
 
         <div className="home__row">
-          {/*product*/}
-          {/*product*/}
-          {/*product*/}
+          <Product
+          id="42553"
+          title="RipTide Surfboard"
+          price={199.99}
+          image="https://www.riptidewatersports.com/wp-content/uploads/surf-board-e1560545921742-1024x461.jpg"
+          rating={5}
+          />
+          <Product
+           id="52342"
+           title="Nike Metcon 5s"
+           price={129.99}
+           image="https://www.roguefitness.com/media/catalog/product/cache/1/rogue_header_2015/750x/472321edac810f9b2465a359d8cdc0b5/a/q/aq1189-002-h.jpg"
+           rating={5}
+          />
+          <Product
+          id="463453"
+          title="Blue Eyes White Dragon"
+          price={999.99}
+          image="https://d1rw89lz12ur5s.cloudfront.net/photo/collectorscache/file/763621/LCKC-EN001D.jpg?1520537161"
+          rating={5}
+          />
         </div>
 
-        <div className="home__row">{/*product*/}</div>
+        <div className="home__row">
+          
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({id,title, image, price, rating}) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
    
         <p className="product__price">
           <small>$</small>
-          <strong>19.99</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          <p>⭐</p>
+          {Array(rating).fill().map((_,i) => (<p>⭐</p>))}
+          
         </div>
       </div>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+      <img src={image} alt="" />
       <button> Add to Basket </button>
     </div>
   );
