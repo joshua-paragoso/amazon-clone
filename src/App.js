1629effc1e0ab3bf655c7033d9d09a2d6059df1f
joshypuu <paragoso90@gmail.com>
2020-09-10 11:16:52 -1000

    added a chekcout page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,34 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     //BEM
-    <div className="app">
-      <Header />
-      <Home />
-      {/*Home */}
-    </div>
+    <Router>
+      <div className="app">
+        
+        <Switch> 
+          
+          {/* goes to checkout page */}
+          <Route path="/checkout">
+            <Header />
+            <h1> I am a checkout customer</h1>
+          </Route>
+          
+          {/* goes to main page */}
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+      
+      </div>
+
+    </Router>
+
   );
 }
 
