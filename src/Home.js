48c43426949c0e2718f664ed025ef2b898d87043
joshypuu <paragoso90@gmail.com>
2020-09-10 08:51:35 -1000

    product now is adjusted

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./Home.css";
 import Product from "./Product";
 function Home() {
-    return (
-        <div className='Home'>
-            <div className="home_-container">
-                <img
-                className="home__image" 
-                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220-.jpg" 
-                alt=""/>
+  return (
+    <div className="Home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220-.jpg"
+          alt=""
+        />
 
-            <div className="home__row">
-                <Product/>
-                {/*product*/}
-            </div>
-
-            <div className="home__row">
-                {/*product*/}
-                {/*product*/}
-                {/*product*/}
-            </div>
-
-            <div className="home__row">
-                {/*product*/}
-      
-            </div>
-
-            </div>
+        <div className="home__row">
+          <Product />
+          {/*product*/}
         </div>
-    )
+
+        <div className="home__row">
+          {/*product*/}
+          {/*product*/}
+          {/*product*/}
+        </div>
+
+        <div className="home__row">{/*product*/}</div>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./Product.css";
 
 function Product() {
-    return (
-        <div className="product">
-            
-            <div className="product__info">
-            <p>The lean startup</p> 
-            
-            <p className="product__price">
-                <small>$</small>
-                <strong>19.99</strong>
-            </p>
-            <div className="product__rating">
-                <p> star </p>
-            </div>
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean startup</p>
+   
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>⭐</p>
         </div>
-        <img src="" alt=""/>
-        </div>
-    )
+      </div>
+      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt="" />
+      <button> Add to Basket </button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
