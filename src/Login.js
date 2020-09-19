1fcb640ef892d1d1559ea5ce344b3255b958d5fc
joshypuu <paragoso90@gmail.com>
2020-09-18 16:35:50 -1000

    added some firebase components

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Login.css'
 import {Link} from "react-router-dom";
-import { SettingsSystemDaydreamOutlined } from '@material-ui/icons';
+
 function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const signIn = e =>{
+        e.preventDefault();
+
+        //some fancy firebase login
+    }
+
+    const register = e =>{
+        e.preventDefault();
+
+        //some fancy firebase register
+        
+    }
     return (
     <div className='login'>
         <Link to ='/'>
@@ -16,10 +32,25 @@ function Login() {
                 <form>
                     <h5>E-mail</h5>
                         {/* <iput type='text' value={email} onChange={e => SettingsSystemDaydreamOutlined(e.targer.value)}/> */}
-                        <input type='test' />
+                        <input 
+                            type='text' 
+                            value={email} 
+                            onChange={e => setEmail(e.target.value)} 
+                        />
+
                     <h5>Password</h5>
-                        <input type='password' />
-                    <button className='login__signInButton'>Sign In</button>
+                        
+                        <input 
+                            type='password' 
+                            value={password} 
+                            onChange={e => setPassword(e.target.value)} 
+                        />
+
+                    <button
+                        type='submit'
+                        onClick={signIn} 
+                        className='login__signInButton'>Sign In
+                    </button>
 
                 </form>
 
@@ -28,7 +59,9 @@ function Login() {
                     see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
                 </p>
 
-                <button className='login__registerButton'>Create your amazon account</button>{/*Register button if you dont have an account */}
+                <button onClick={register} className='login__registerButton'>
+                    Create your amazon account
+                </button>{/*Register button if you dont have an account */}
         </div>
 
         
