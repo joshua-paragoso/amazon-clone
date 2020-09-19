7b271885900a7a49315b3226caaccb73471a601e
joshypuu <paragoso90@gmail.com>
2020-09-18 15:08:08 -1000

    Added login and password entries

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Login.css'
 import {Link} from "react-router-dom";
+import { SettingsSystemDaydreamOutlined } from '@material-ui/icons';
 function Login() {
     return (
     <div className='login'>
@@ -13,10 +14,19 @@ function Login() {
             <h1>Sign in</h1>
 
                 <form>
-                    <h5>
-                        
-                    </h5>
+                    <h5>E-mail</h5>
+                        {/* <iput type='text' value={email} onChange={e => SettingsSystemDaydreamOutlined(e.targer.value)}/> */}
+                    <input type='test' />
+                    <h5>Password</h5>
+                    <input type='password' />
+                    <button>Sign In</button>
+
                 </form>
+
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
         </div>
 
         
