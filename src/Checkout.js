4d19dea2384c11b27f9b7b830a1d985c5f10be79
joshypuu <paragoso90@gmail.com>
2020-09-24 11:34:10 -1000

    checkout now greets user with email address

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal"
 
 function Checkout(){
-    const [{ basket }, dispatch] = useStateValue();
+    const [{ basket, user }, dispatch] = useStateValue();
     return (
 
     <div className="checkout">
@@ -16,6 +16,10 @@ function Checkout(){
             /> 
 
             <div>
+                <h3>
+                    {/* displays user email at checkout */}
+                    Hello {user?.email}
+                </h3>
                 <h2 className="checkout__title">
                     Your shopping basket
                 </h2>
