0eeb14bf8dffaa81069d4d1ad68e7a4c6f1ccb36
joshypuu <paragoso90@gmail.com>
2020-10-02 08:45:17 -1000

    removed button from order page

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './CheckoutProduct.css'
 import { useStateValue } from './StateProvider'
 
-function CheckoutProduct( {id, image, title, price, rating}) {
+function CheckoutProduct( {id, image, title, price, rating, hideButton}) {
 
     const [{basket}, dispatch] = useStateValue();
 
@@ -38,7 +38,9 @@ function CheckoutProduct( {id, image, title, price, rating}) {
                 ))}
             </div>
 
+            {!hideButton && (
             <button onClick={removeFromBasket}>Remove From Basket</button>
+            )}
             </div>
             
         </div>
