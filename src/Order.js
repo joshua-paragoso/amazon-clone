06ec4ee536179fe8d87b9f7c80081964270ac99c
joshypuu <paragoso90@gmail.com>
2020-10-02 08:32:31 -1000

    added currency format to order

--- src/Order.js
+++ src/Order.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Order.css'
 import moment from "moment";
 import CheckoutProduct from "./CheckoutProduct";
+import CurrencyFormat from "react-currency-format";
 
 function Order( {order}) {
     return (
@@ -22,6 +23,17 @@ function Order( {order}) {
                     hideButton
                 />
             ))}
+
+        <CurrencyFormat
+                renderText={(value) => (
+                    <h3 className="order__total">Order Total: {value}</h3>
+                )}
+                decimalScale={2}
+                value={order.data.amount / 100}
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+        /> 
 
         </div>
     )
